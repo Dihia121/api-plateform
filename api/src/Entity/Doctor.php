96481c8db30b570544f9e7016929cffbdceb3508
Dihia121 <siliana.aitsmail@gmail.com>
2021-12-14 14:07:46 +0100

    entitycreation: doctor

---
+++ api/src/Entity/Doctor.php
@@ -0,0 +1,202 @@
+<?php
+
+namespace App\Entity;
+
+use ApiPlatform\Core\Annotation\ApiResource;
+use App\Repository\DoctorRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: DoctorRepository::class)]
+#[ApiResource]
+class Doctor
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column(type: 'integer')]
+    private $id;
+
+    #[ORM\Column(type: 'string', length: 255)]
+    private $name;
+
+    #[ORM\Column(type: 'string', length: 255)]
+    private $lastname;
+
+    #[ORM\Column(type: 'string', length: 255)]
+    private $gender;
+
+    #[ORM\Column(type: 'string', length: 255)]
+    private $streetName;
+
+    #[ORM\Column(type: 'string', length: 255)]
+    private $streetAdress;
+
+    #[ORM\Column(type: 'string', length: 255)]
+    private $postalCode;
+
+    #[ORM\Column(type: 'string', length: 255)]
+    private $city;
+
+    #[ORM\Column(type: 'string', length: 255)]
+    private $country;
+
+    #[ORM\Column(type: 'string', length: 255)]
+    private $homePhone;
+
+    #[ORM\Column(type: 'string', length: 10)]
+    private $workPhone;
+
+    #[ORM\Column(type: 'string', length: 10)]
+    private $mobilePhone;
+
+    #[ORM\Column(type: 'string', length: 255)]
+    private $emailAdress;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getLastname(): ?string
+    {
+        return $this->lastname;
+    }
+
+    public function setLastname(string $lastname): self
+    {
+        $this->lastname = $lastname;
+
+        return $this;
+    }
+
+    public function getGender(): ?string
+    {
+        return $this->gender;
+    }
+
+    public function setGender(string $gender): self
+    {
+        $this->gender = $gender;
+
+        return $this;
+    }
+
+    public function getStreetName(): ?string
+    {
+        return $this->streetName;
+    }
+
+    public function setStreetName(string $streetName): self
+    {
+        $this->streetName = $streetName;
+
+        return $this;
+    }
+
+    public function getStreetAdress(): ?string
+    {
+        return $this->streetAdress;
+    }
+
+    public function setStreetAdress(string $streetAdress): self
+    {
+        $this->streetAdress = $streetAdress;
+
+        return $this;
+    }
+
+    public function getPostalCode(): ?string
+    {
+        return $this->postalCode;
+    }
+
+    public function setPostalCode(string $postalCode): self
+    {
+        $this->postalCode = $postalCode;
+
+        return $this;
+    }
+
+    public function getCity(): ?string
+    {
+        return $this->city;
+    }
+
+    public function setCity(string $city): self
+    {
+        $this->city = $city;
+
+        return $this;
+    }
+
+    public function getCountry(): ?string
+    {
+        return $this->country;
+    }
+
+    public function setCountry(string $country): self
+    {
+        $this->country = $country;
+
+        return $this;
+    }
+
+    public function getHomePhone(): ?string
+    {
+        return $this->homePhone;
+    }
+
+    public function setHomePhone(string $homePhone): self
+    {
+        $this->homePhone = $homePhone;
+
+        return $this;
+    }
+
+    public function getWorkPhone(): ?string
+    {
+        return $this->workPhone;
+    }
+
+    public function setWorkPhone(string $workPhone): self
+    {
+        $this->workPhone = $workPhone;
+
+        return $this;
+    }
+
+    public function getMobilePhone(): ?string
+    {
+        return $this->mobilePhone;
+    }
+
+    public function setMobilePhone(string $mobilePhone): self
+    {
+        $this->mobilePhone = $mobilePhone;
+
+        return $this;
+    }
+
+    public function getEmailAdress(): ?string
+    {
+        return $this->emailAdress;
+    }
+
+    public function setEmailAdress(string $emailAdress): self
+    {
+        $this->emailAdress = $emailAdress;
+
+        return $this;
+    }
+}
